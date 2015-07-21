73d71f2e0f7304f490301de04461bd2ad6b727c6
xc43 <iovg75@gmail.com>
2015-07-21 16:48:19 -0400

    Inclusion du code selection sort v 0.1

--- tp5_ift436/tp5_ift436.cpp
+++ tp5_ift436/tp5_ift436.cpp
@@ -1,11 +1,23 @@
-// tp5_ift436.cpp : définit le point d'entrée pour l'application console.
+// tp5_ift436.cppÂ : dÃ©finit le point d'entrÃ©e pour l'application console.
 //
 
 #include "stdafx.h"
-
-
-int _tmain(int argc, _TCHAR* argv[])
-{
-	return 0;
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+ 
+template<typename ForwardIterator> void selection_sort(ForwardIterator begin,
+                                                       ForwardIterator end) {
+  for(auto i = begin; i != end; ++i) {
+    std::iter_swap(i, std::min_element(i, end));
+  }
 }
+
+int main() {
+  int a[] = {100, 2, 56, 200, -52, 3, 99, 33, 177, -199};
+  selection_sort(std::begin(a), std::end(a));
+  copy(std::begin(a), std::end(a), std::ostream_iterator<int>(std::cout, " "));
+  std::cout << "\n";
+}
+
 
