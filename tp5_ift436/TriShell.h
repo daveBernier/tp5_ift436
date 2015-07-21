db1c391844c334502fc4a7e03fa27e6f43a22c9d
xc43 <iovg75@gmail.com>
2015-07-21 19:29:40 -0400

    tri shell fini

--- tp5_ift436/TriShell.h
+++ tp5_ift436/TriShell.h
@@ -2,42 +2,44 @@
  
 using namespace std;
 
-class shell
+class TriShell
 {
 public:
-    shell() 
+    TriShell()
     { _gap[0] = 1750; _gap[1] = 701; _gap[2] = 301; _gap[3] = 132; _gap[4] = 57; _gap[5] = 23; _gap[6] = 10; _gap[7] = 4; _gap[8] = 1; }
- 
-    void sort( int* a, int count )
+
+    vector<int> sort(vector<int> a)
     {
-	_cnt = count;
+
+	_cnt = a.size();
 	for( int x = 0; x < 9; x++ )
-	    if( count > _gap[x] )
+	    if( _cnt > _gap[x] )
 	    { _idx = x; break; }
- 
-	sortIt( a );
+
+	sortIt(a );
+	return a;
     }
- 
-private:	
-    void sortIt( int* arr )
+
+private:
+    void sortIt(vector<int> arr )
     {
-	bool sorted = false;
-	while( true )
-	{
-	    sorted = true;
-	    int st = 0;
-	    for( int x = _gap[_idx]; x < _cnt; x += _gap[_idx] )
-	    {
-		if( arr[st] > arr[x] )
-		{ swap( arr[st], arr[x] ); sorted = false; }
-		st = x;
-	    }
-	    if( ++_idx >= 8 ) _idx = 8;
-	    if( sorted && _idx == 8 ) break;
-	}
+        bool sorted = false;
+        while( true )
+        {
+            sorted = true;
+            int st = 0;
+            for( int x = _gap[_idx]; x < _cnt; x += _gap[_idx] )
+            {
+            if( arr[st] > arr[x] )
+            { swap( arr[st], arr[x] ); sorted = false; }
+            st = x;
+            }
+            if( ++_idx >= 8 ) _idx = 8;
+            if( sorted && _idx == 8 ) break;
+        }
     }
- 
+
     void swap( int& a, int& b ) { int t = a; a = b; b = t; }
- 
+
     int _gap[9], _idx, _cnt;
 };
